c2b202b903286b5bf7b2fec0c736f67983afdc49
Abdullah2Cool <abdullahbinasad14@gmail.com>
2017-05-07 13:50:57 -0400

    Initial Commit

--- core/src/maze/maker/Main.java
+++ core/src/maze/maker/Main.java
@@ -29,7 +29,7 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         shape = new ShapeRenderer();
-        scl = 10; //////// INCREASE THIS NUMBER TO MAKE THE MAZE SMALLER
+        scl = 10; /////// INCREASE THIS NUMBER TO MAKE THE MAZE SMALLER
         rows = fWidth / scl;
         cols = fHeight / scl;
         Grid = createGrid();
